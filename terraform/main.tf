89f36e3ae8c4f11ad56ec6ee703b84d902892f25
MARCELO GUERRERO <32203163+croguerrero@users.noreply.github.com>
2022-04-19 18:39:55 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,23 @@
 terraform {
-  required_version = ">= 0.12.26"
+  required_providers {
+    docker = {
+       source = "terraform-providers/docker"
+    }
+  }
+  
+}
+provider "docker"{}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
 }
 
-variable "subject" {
-   type = string
-   default = "World"
-   description = "Subject to hello"
-}
-
-output "hello_world" {
-  value = "Hello, ${var.subject}!"
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8081
+  }
 }
