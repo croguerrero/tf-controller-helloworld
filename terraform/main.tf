98f458c8958d16a5244be109e7925ff7be65fa50
MARCELO GUERRERO <32203163+croguerrero@users.noreply.github.com>
2022-04-19 19:02:44 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,13 +2,62 @@ terraform {
  required_version = ">= 0.12.26"
  }
   
-provider "kubernetes" {
-  config_path    = "~/.kube/config"
-  config_context = "my-context"
-}
-
-resource "kubernetes_namespace" "example" {
+resource "kubernetes_deployment" "example" {
   metadata {
-    name = "production"
+    name = "terraform-example"
+    labels = {
+      test = "MyExampleApp"
+    }
+  }
+
+  spec {
+    replicas = 3
+
+    selector {
+      match_labels = {
+        test = "MyExampleApp"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          test = "MyExampleApp"
+        }
+      }
+
+      spec {
+        container {
+          image = "nginx:1.21.6"
+          name  = "example"
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/"
+              port = 80
+
+              http_header {
+                name  = "X-Custom-Header"
+                value = "Awesome"
+              }
+            }
+
+            initial_delay_seconds = 3
+            period_seconds        = 3
+          }
+        }
+      }
+    }
   }
 }
