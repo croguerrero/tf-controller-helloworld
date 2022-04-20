898e1c490e02927da72766ce74653f7c9f0f4735
MARCELO GUERRERO <32203163+croguerrero@users.noreply.github.com>
2022-04-19 19:06:15 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,11 @@
 terraform {
  required_version = ">= 0.12.26"
  }
-  
+
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "my-context"
+}
 resource "kubernetes_deployment" "example" {
   metadata {
     name = "terraform-example"
